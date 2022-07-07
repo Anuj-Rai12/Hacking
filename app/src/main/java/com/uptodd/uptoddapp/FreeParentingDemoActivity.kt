59a5040979ac6d5b2b9c4e60bf843d1f742100a8
Anuj <anujrai04082002.0@gmail.com>
2022-07-07 18:09:27 +0530

    Added the Custom Layout status:Success

--- app/src/main/java/com/uptodd/uptoddapp/FreeParentingDemoActivity.kt
+++ app/src/main/java/com/uptodd/uptoddapp/FreeParentingDemoActivity.kt
@@ -1,13 +1,9 @@
 package com.uptodd.uptoddapp
 
 import android.os.Bundle
-import android.view.Gravity
-import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.uptodd.uptoddapp.databinding.ActvityFreeDemoBinding
-import com.uptodd.uptoddapp.databinding.FreeParentingActionBarBinding
-import com.uptodd.uptoddapp.utils.toastMsg
 
 class FreeParentingDemoActivity : AppCompatActivity() {
 
@@ -20,32 +16,9 @@ class FreeParentingDemoActivity : AppCompatActivity() {
     }
 
 
-    fun setUpCustomActionBar(title: String, btnClick: () -> Unit) {
-        val toolBinding = FreeParentingActionBarBinding.inflate(layoutInflater)
-        supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
-        supportActionBar?.setDisplayShowCustomEnabled(true)
-        val lp: ActionBar.LayoutParams =
-            ActionBar.LayoutParams(
-                ActionBar.LayoutParams.MATCH_PARENT,
-                ActionBar.LayoutParams.WRAP_CONTENT
-            )
-        lp.gravity = Gravity.LEFT
-        supportActionBar?.setCustomView(toolBinding.root, lp)
-
-        toolBinding.topAppBar.setContentInsetsAbsolute(0, 0)
-
-        toolBinding.topAppBar.setNavigationOnClickListener {
-            toastMsg("Working ...")
-            btnClick.invoke()
-        }
-    }
-
-
     override fun onResume() {
         super.onResume()
-        supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_TITLE
-        supportActionBar?.setDisplayShowCustomEnabled(false)
-        supportActionBar?.title = getString(R.string.app_name)
+        supportActionBar?.hide()
     }
 
 }
--- app/src/main/java/com/uptodd/uptoddapp/ui/freeparenting/login/ParentingLoginFragment.kt
+++ app/src/main/java/com/uptodd/uptoddapp/ui/freeparenting/login/ParentingLoginFragment.kt
@@ -2,13 +2,10 @@ package com.uptodd.uptoddapp.ui.freeparenting.login
 
 import android.os.Bundle
 import android.view.View
-import androidx.appcompat.app.ActionBar
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import com.uptodd.uptoddapp.FreeParentingDemoActivity
 import com.uptodd.uptoddapp.R
 import com.uptodd.uptoddapp.databinding.LoginParentingFragmentBinding
-import com.uptodd.uptoddapp.utils.hide
 import com.uptodd.uptoddapp.utils.invisible
 
 class ParentingLoginFragment : Fragment(R.layout.login_parenting_fragment) {
@@ -17,24 +14,10 @@ class ParentingLoginFragment : Fragment(R.layout.login_parenting_fragment) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = LoginParentingFragmentBinding.bind(view)
-        (activity as FreeParentingDemoActivity).setUpCustomActionBar("Login") {
-
-        }
-
         binding.goToDemoDashBoard.setOnClickListener {
             binding.goToDemoDashBoard.invisible()
             binding.pbBtn.isVisible = true
         }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        (activity as FreeParentingDemoActivity).supportActionBar?.displayOptions =
-            ActionBar.DISPLAY_SHOW_TITLE
-        (activity as FreeParentingDemoActivity).supportActionBar?.setDisplayShowCustomEnabled(
-            false
-        )
-        (activity as FreeParentingDemoActivity).supportActionBar?.title = "Login"
     }
 
 
