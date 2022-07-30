ffb041b4f56ff03ab7f5796554fed765c6d9fe44
Anuj <anujrai04082002.0@gmail.com>
2022-07-30 23:43:59 +0530

    Removed Some Bug Order List Fragment status:Success

--- app/src/main/java/com/uptodd/uptoddapp/ui/order/orderlist/OrderListFragment.kt
+++ app/src/main/java/com/uptodd/uptoddapp/ui/order/orderlist/OrderListFragment.kt
@@ -9,6 +9,7 @@ import android.graphics.drawable.ColorDrawable
 import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.text.TextUtils
 import android.util.Log
 import android.view.LayoutInflater
@@ -19,7 +20,6 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
@@ -142,7 +142,7 @@ class OrderListFragment : Fragment() {
 
     private fun initObservers() {
         //Observing for data
-        viewModel.allOrderList.observe(viewLifecycleOwner, Observer {
+        viewModel.allOrderList.observe(viewLifecycleOwner) {
             if (it != null) {
                 Log.d(
                     "div",
@@ -158,10 +158,10 @@ class OrderListFragment : Fragment() {
                 }
                 displayOrders(viewModel.allOrderList.value!!)
             }
-        })
+        }
 
         //Observing for extendSubscription request
-        viewModel.isLoadingDialogVisible.observe(viewLifecycleOwner, Observer {
+        viewModel.isLoadingDialogVisible.observe(viewLifecycleOwner) {
             Log.d("div", "OrderListFragment L153 ${viewModel.isLoadingDialogVisible.value}")
             if (!it) {
                 if (viewModel.isExtendSubscriptionRequestMade) {
@@ -175,18 +175,18 @@ class OrderListFragment : Fragment() {
                         })
                 }
             }
-        })
+        }
 
-        viewModel.shouldShowBookingButton.observe(viewLifecycleOwner, Observer {
+        viewModel.shouldShowBookingButton.observe(viewLifecycleOwner) {
             if (!it)
                 binding.sessionCount.visibility = View.VISIBLE
             else
                 binding.sessionCount.visibility = View.GONE
             shouldShowButton = !it
-        })
-        viewModel.bookingLink.observe(viewLifecycleOwner, Observer {
+        }
+        viewModel.bookingLink.observe(viewLifecycleOwner) {
             checkForButton(it)
-        })
+        }
     }
 
     private fun loadData() {
@@ -304,14 +304,14 @@ class OrderListFragment : Fragment() {
                 showLoadingDialog()
                 viewModel.requestExtendSubscription()
 
-                viewModel.isLoadingDialogVisible.observe(viewLifecycleOwner, Observer {
+                viewModel.isLoadingDialogVisible.observe(viewLifecycleOwner) {
                     Log.d("div", "OrderListFragment L153 ${viewModel.isLoadingDialogVisible.value}")
                     if (!it) {
                         if (viewModel.isExtendSubscriptionRequestMade) {
                             dialog.dismiss()
                         }
                     }
-                })
+                }
 
             } else {
                 //showInternetNotConnectedDialog()
@@ -373,12 +373,12 @@ class OrderListFragment : Fragment() {
                     }
                 }
             })
-        viewModel.isLoadingDialogVisible.observe(viewLifecycleOwner, Observer {
+        viewModel.isLoadingDialogVisible.observe(viewLifecycleOwner) {
             if (!it) {
                 upToddDialogs.dismissDialog()
             }
-        })
-        val handler = Handler()
+        }
+        val handler = Handler(Looper.getMainLooper())
         handler.postDelayed({
             upToddDialogs.dismissDialog()
         }, R.string.loadingDuarationInMillis.toLong())
