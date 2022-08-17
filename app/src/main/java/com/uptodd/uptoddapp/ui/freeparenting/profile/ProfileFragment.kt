18a766586ff2038637f606da1a5a25fb54cd5359
Anuj <anujrai04082002.0@gmail.com>
2022-08-18 02:10:40 +0530

    Added User Credential to Profile screen status:Successfully

--- app/src/main/java/com/uptodd/uptoddapp/ui/freeparenting/profile/ProfileFragment.kt
+++ app/src/main/java/com/uptodd/uptoddapp/ui/freeparenting/profile/ProfileFragment.kt
@@ -1,23 +1,56 @@
 package com.uptodd.uptoddapp.ui.freeparenting.profile
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.uptodd.uptoddapp.FreeParentingDemoActivity
 import com.uptodd.uptoddapp.R
 import com.uptodd.uptoddapp.databinding.ProfileLayoutFragmentBinding
+import com.uptodd.uptoddapp.datamodel.freeparentinglogin.LoginSingletonResponse
+import com.uptodd.uptoddapp.utils.getEmojiByUnicode
 import com.uptodd.uptoddapp.utils.hide
 
 class ProfileFragment : Fragment(R.layout.profile_layout_fragment) {
 
     private lateinit var binding: ProfileLayoutFragmentBinding
+    private val loginSingletonResponse by lazy {
+        LoginSingletonResponse.getInstance()
+    }
+
+    private val countryCode = mutableSetOf(
+        "${getEmojiByUnicode(0x1F1EE)}${getEmojiByUnicode(0x1F1F3)} +91",
+        "${getEmojiByUnicode(0x1F1F9)}${getEmojiByUnicode(0x1F1ED)} +66",
+        "${getEmojiByUnicode(0x1F1E6)}${getEmojiByUnicode(0x1F1EA)} +971",
+    )
+
+    private val dropDownArray: ArrayAdapter<String> by lazy {
+        ArrayAdapter(requireContext(), R.layout.dropdown, countryCode.toTypedArray())
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = ProfileLayoutFragmentBinding.bind(view)
-
+        binding.userTitle.text = loginSingletonResponse.getLoginRequest()?.name
+        binding.userNameEd.setText(loginSingletonResponse.getLoginRequest()?.name)
+        binding.userEmailEd.setText(loginSingletonResponse.getLoginRequest()?.email)
+        binding.userPhoneEd.setText(loginSingletonResponse.getLoginRequest()?.phone)
+        binding.countryCodeEd.setAdapter(dropDownArray)
+        val str=countryCode.find {
+            it.contains(loginSingletonResponse.getLoginRequest()?.mobileCode!!)
+        }
+        if (countryCode.contains(str)){
+            val index=countryCode.indexOf(str)
+            binding.countryCodeEd.setText(countryCode.elementAt(index),false)
+        }
+        binding.toolbarNav.topAppBar.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onResume() {
         super.onResume()
         (activity as FreeParentingDemoActivity?)?.hideBottomNavBar()
